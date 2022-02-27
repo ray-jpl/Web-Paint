8055a375a0ad1f78129a478e49f344cd21e01edd
ray-jpl <liraymond4@gmail.com>
2022-02-27 17:06:42 +1100

    Fixed grid DragDrop event issue and not colouring on cell click

--- paint.js
+++ paint.js
@@ -1,4 +1,16 @@
+
+
+const DEFAULT_SIZE = 16
+
 var grid = document.getElementById("grid");
+var clearBtn = document.getElementById("clearBtn")
+
+// Prevents ghosting image when drag/drop is attempted by mousedown Event
+grid.ondragstart = function() {
+    return false;
+};
+
+
 
 function createGrid(size) {
     grid.style.setProperty('--grid-rows', size);
@@ -6,16 +18,27 @@ function createGrid(size) {
     for (var rows=0; rows < size; rows++) {
         for (var col=0; col< size; col++) {
             var cell = document.createElement("div")
+            cell.addEventListener('mousedown', colourIn);
+            cell.addEventListener('mouseover', colourIn);
             grid.appendChild(cell).className="cell";
         }
     }
 }
 
-createGrid(16)
+
+createGrid(DEFAULT_SIZE)
+let colourOn = false;
+document.body.onmousedown = () => {colourOn = true};
+document.body.onmouseup = () => (colourOn = false);
 
 
-grid.addEventListener('mouseover', function(e) {
-    if (e.target.matches('.cell')) {
-        e.target.classList.add('colored');
+function colourIn(e) {
+    // If mouse is clicked then it will fill
+    // or if mouse is already held down then it will fill.
+    // Checking only if the mouse is held down does not colour in the cell
+    // where the mouse clicked down
+    if (e.type == 'mousedown' || colourOn) {
+        e.target.style.backgroundColor = '#333333';
     }
-});
+}
+
